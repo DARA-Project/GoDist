a5db5a816d93ea4fe67901141bcf496f8d3abf73
Vaastav Anand <vaastav.anand05@gmail.com>
2019-04-15 23:08:23 -0700

    Capture sleep system call

--- src/runtime/time.go
+++ src/runtime/time.go
@@ -9,6 +9,7 @@ package runtime
 import (
 	"runtime/internal/sys"
 	"unsafe"
+    "dara"
 )
 
 // Package time knows the layout of this structure.
@@ -82,6 +83,12 @@ var faketime int64
 // timeSleep puts the current goroutine to sleep for at least ns nanoseconds.
 //go:linkname timeSleep time.Sleep
 func timeSleep(ns int64) {
+    if Is_dara_profiling_on() {
+        println("[SLEEP]", ns)
+        argInfo := dara.GeneralType{Type: dara.INTEGER64, Integer64: ns}
+        syscallInfo := dara.GeneralSyscall{dara.DSYS_SLEEP, 1, 0, [10]dara.GeneralType{argInfo}, [10]dara.GeneralType{}}
+        Report_Syscall_To_Scheduler(dara.DSYS_SLEEP, syscallInfo)
+    }
 	if ns <= 0 {
 		return
 	}
